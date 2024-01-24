233aea9922001d3a50044b9f7a8df9a97d93f49e
McMiguel2004 <mgomezalcantara@elpuig.xeill.net>
2024-01-24 20:18:52 +0100

    Mig

---
+++ app/src/main/java/com/example/instahub/Post.java
@@ -0,0 +1,23 @@
+package com.example.instahub;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Post {
+    public String uid;
+    public String author;
+    public String authorPhotoUrl;
+    public String content;
+
+    public Map<String, Boolean> likes = new HashMap<>();
+
+    // Constructor vacio requerido por Firestore
+    public Post() {}
+
+    public Post(String uid, String author, String authorPhotoUrl, String content) {
+        this.uid = uid;
+        this.author = author;
+        this.authorPhotoUrl = authorPhotoUrl;
+        this.content = content;
+    }
+}
